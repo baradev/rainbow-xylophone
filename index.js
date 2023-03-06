66f4ce75c117e1cde7ebe3931e0e4c0fb51166c7
kbarushkaa <barboraklusackova@gmail.com>
2023-03-07 09:54:26 +1300

    xylophone

--- index.js
+++ index.js
@@ -1,71 +1,67 @@
-for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
-
-  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-
+for (var i = 0; i < document.querySelectorAll(".note").length; i++) {
+  document.querySelectorAll(".note")[i].addEventListener("click", function () {
     var buttonInnerHTML = this.innerHTML;
     makeSound(buttonInnerHTML);
     buttonAnimation(buttonInnerHTML);
   });
-
-
 }
 
-
-document.addEventListener("keydown", function(event){
-makeSound(event.key);
-buttonAnimation(event.key);
+document.addEventListener("keydown", function (event) {
+  makeSound(event.key);
+  buttonAnimation(event.key);
 });
 
-
-function makeSound(key){
+function makeSound(key) {
   switch (key) {
-    case "w":
-      var tom4 = new Audio('sounds/tom-4.mp3');
-      tom4.play();
-        break;
-
-    case "a":
-      var tom3 = new Audio('sounds/tom-3.mp3');
-      tom3.play();
-        break;
-
-    case "s":
-    var tom2 = new Audio('sounds/tom-2.mp3');
-    tom2.play();
+    case "c":
+      var red = new Audio("sounds/c-tune.wav");
+      red.play();
       break;
 
     case "d":
-    var tom1 = new Audio('sounds/tom-1.mp3');
-    tom1.play();
+      var orange = new Audio("sounds/d-tune.wav");
+      orange.play();
       break;
 
-    case "j":
-    var snare = new Audio('sounds/snare.mp3');
-    snare.play();
+    case "e":
+      var yellow = new Audio("sounds/e-tune.wav");
+      yellow.play();
       break;
 
-    case "k":
-    var kick = new Audio('sounds/kick-bass.mp3');
-    kick.play();
+    case "f":
+      var green = new Audio("sounds/f-tune.wav");
+      green.play();
       break;
 
-    case "l":
-    var crash = new Audio('sounds/crash.mp3');
-    crash.play();
+    case "g":
+      var green2 = new Audio("sounds/g-tune.wav");
+      green2.play();
       break;
 
-    default:console.log(buttonInnerHTML);
+    case "a":
+      var blue = new Audio("sounds/a-tune.wav");
+      blue.play();
+      break;
 
+    case "b":
+      var purple = new Audio("sounds/b-tune.wav");
+      purple.play();
+      break;
+
+    case "2":
+      var pink = new Audio("sounds/c2-tune.wav");
+      pink.play();
+      break;
+
+    default:
+      console.log(buttonInnerHTML);
   }
 }
 
-
-function buttonAnimation (currentKey) {
-var activeButton = document.querySelector("." + currentKey);
-activeButton.classList.add("pressed");
-setTimeout (function() {
-  activeButton.classList.remove("pressed")
-
-}, 200)
-
+function buttonAnimation(currentKey) {
+  var activeButton = document.querySelector("." + currentKey);
+  activeButton.classList.add("pressed");
+  setTimeout(function () {
+    activeButton.classList.remove("pressed");
+  }, 200);
 }
