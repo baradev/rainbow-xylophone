77a84d65678863fc15307ab11e8e689dd00b218a
kbarushkaa <barboraklusackova@gmail.com>
2023-04-06 23:19:48 +1200

    switching from letters to numbers

--- index.js
+++ index.js
@@ -13,42 +13,42 @@ document.addEventListener("keydown", function (event) {
 
 function makeSound(key) {
   switch (key) {
-    case "c":
+    case "1":
       var red = new Audio("sounds/c-tune.wav");
       red.play();
       break;
 
-    case "d":
+    case "2":
       var orange = new Audio("sounds/d-tune.wav");
       orange.play();
       break;
 
-    case "e":
+    case "3":
       var yellow = new Audio("sounds/e-tune.wav");
       yellow.play();
       break;
 
-    case "f":
+    case "4":
       var green = new Audio("sounds/f-tune.wav");
       green.play();
       break;
 
-    case "g":
+    case "5":
       var green2 = new Audio("sounds/g-tune.wav");
       green2.play();
       break;
 
-    case "a":
+    case "6":
       var blue = new Audio("sounds/a-tune.wav");
       blue.play();
       break;
 
-    case "b":
+    case "7":
       var purple = new Audio("sounds/b-tune.wav");
       purple.play();
       break;
 
-    case "2":
+    case "8":
       var pink = new Audio("sounds/c2-tune.wav");
       pink.play();
       break;
@@ -59,7 +59,7 @@ function makeSound(key) {
 }
 
 function buttonAnimation(currentKey) {
-  var activeButton = document.querySelector("." + currentKey);
+  var activeButton = document.querySelector(".x" + currentKey);
   activeButton.classList.add("pressed");
   setTimeout(function () {
     activeButton.classList.remove("pressed");
